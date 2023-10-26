bf7b414f03f8667ad77453815dff5e11ed77e34e
hermy1 <49494077+hermy1@users.noreply.github.com>
2023-10-25 19:28:08 -0500

    add Courses

--- src/models/Course.ts
+++ src/models/Course.ts
@@ -5,16 +5,16 @@ import { ObjectId } from "mongodb";
  class name is not used because of the name conflict with the class keyword in javascript
 */
 
-export interface Course {
-    _id: ObjectId;
-    courseCode: string;
-    courseName: string;
-    courseDescription: string;
-    courseDepartment: string;
-    professorId: ObjectId;
+export class Course {
+    _id: ObjectId = new ObjectId();
+    courseCode: string = '';
+    courseName: string = '';
+    courseDescription: string = '';
+    courseDepartment: string =  '';
+    professorId: ObjectId = new ObjectId();
     //students is an array of ObjectIds
-    students: ObjectId[];
+    students: ObjectId[] = [];
     // schudule: string; MWF 10:00-11:00
-    createdAt: Date;
-    updatedAt: Date;
+    createdAt: Date = new Date();
+    updatedAt: Date = new Date();
 }
