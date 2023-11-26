1ef7dbeecf34cb6fe0cf147f91e855907df9a615
hermy1 <49494077+hermy1@users.noreply.github.com>
2023-11-26 03:50:42 +0000

    cors hardcoded line removed

--- src/main.ts
+++ src/main.ts
@@ -46,8 +46,7 @@ export async function main(options: MainOptions) {
     //set up cors 
     app.use(
       cors({
-        //origin: config.corsOrigin,
-        origin: ['http://localhost:19006'],
+        origin: config.corsOrigin,
         credentials: true,
       })
     );
