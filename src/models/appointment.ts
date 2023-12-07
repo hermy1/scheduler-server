cc0e5004d4771de72977532a5d7e74e79939e267
hermy1 <49494077+hermy1@users.noreply.github.com>
2023-12-06 18:16:22 -0600

    Revert "get student name with appt"

--- src/models/appointment.ts
+++ src/models/appointment.ts
@@ -31,24 +31,3 @@ export class Appointment {
     updatedAt: Date = new Date();
     studentCancelled: boolean = false; //true if they decide to cancel the meeting
 }
-
-export class FullAppointment{
-    _id: ObjectId = new ObjectId();
-    student: ObjectId = new ObjectId();
-    studentName: string | undefined;
-    professor: ObjectId = new ObjectId();
-    professorName: string | undefined;
-    startDateTime: Date = new Date();
-    endDateTime: Date = new Date();
-    advisor: boolean = false;
-    status: AppointmentStatus = AppointmentStatus.Pending;
-    reason?: string = ""; // message or reason for appointment
-    location?: string = ""; // office location
-    guest?: Guest = new Guest(); // guest professor 
-    guestName?: string;
-    secondaryStatus?: AppointmentStatus | string = ""; // guest professor response
-    summary?: string = ""; // short description/summary of the appointment
-    createdAt: Date = new Date();
-    updatedAt: Date = new Date();
-    studentCancelled: boolean = false; //true if they decide to cancel the meeting
-}
