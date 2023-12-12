04c42ef4d657e2ed9ef6058e0920886219343e92
Hannah Kuderer <hannah.kuderer@msj.edu>
2023-12-11 19:22:53 -0500

    limit to 10 notifications

--- src/mongo/queries/notification.ts
+++ src/mongo/queries/notification.ts
@@ -3,22 +3,24 @@ import { ensureObjectId, getDB } from "../../core/config/utils/mongohelper";
 import { Notification } from "../../models/notification";
 import { MongoFindError } from "../../core/errors/mongo";
 
-export const allNotifications = async (id:string|ObjectId): Promise<Notification[]> => {
-    try {
-      const db = await getDB();
-      const collection = await db.collection<Notification>('notifications');
-    let results = collection.find({userId: ensureObjectId(id)}).toArray();
-  if(results){
-    (await results).reverse();
+export const allNotifications = async (id: string | ObjectId): Promise<Notification[]> => {
+  try {
+    const db = await getDB();
+    const collection = await db.collection<Notification>('notifications');
+    
+    // Use ensureObjectId(id) to convert the id to ObjectId if needed
+    let results = await collection
+      .find({ userId: ensureObjectId(id) })
+      .sort({ createdAt: -1 }) // Sorting in descending order based on createdAt field
+      .limit(10) // Limiting the number of results to 10
+      .toArray();
 
-      return results;
-  }else {
-    throw new MongoFindError("Could not find notifications for user");
+    return results;
+  } catch (err) {
+    throw err;
   }
-    } catch (err) {
-      throw err;
-    }
-  };
+};
+
 
   export const getNotification = async (id:string|ObjectId): Promise<Notification[]> => {
     try {
